b43ddabda7619cd11f89b64d416a04e6747f763f
Jiaming Liu <storypku@gmail.com>
2022-12-09 23:25:02 +0800

    Unlink some bad symlinks

--- tests/scripts/x_env_setup.sh
+++ tests/scripts/x_env_setup.sh
@@ -2,10 +2,39 @@
 set -euo pipefail
 set -x
 
+# To avoid the "Infinite symlink expansion" problem described below 
+# for the `local_cuda_cross` repository rule:
+# 
+# ```
+# ERROR: infinite symlink expansion detected
+# [start of symlink chain]
+# /l4t/targetfs/run/speech-dispatcher
+# /l4t/targetfs/run/speech-dispatcher/.cache/speech-dispatcher
+# [end of symlink chain]
+# ERROR: Infinite symlink expansion, for sysroot_stub/run/speech-dispatcher/.cache/speech-dispatcher, skipping: Infinite symlink expansion
+# ERROR: infinite symlink expansion detected
+# [start of symlink chain]
+# /l4t/targetfs/run/speech-dispatcher
+# /l4t/targetfs/run/speech-dispatcher/.speech-dispatcher
+# [end of symlink chain]
+# ```
+# 
+# We should run:
+# 
+# ```
+# sudo unlink /l4t/targetfs/usr/bin/X11
+# sudo unlink /l4t/targetfs/run/speech-dispatcher/.cache/speech-dispatcher
+# sudo unlink /l4t/targetfs/run/speech-dispatcher/.speech-dispatcher
+# ```
+
 function extract_targetfs_and_toolchain {
   pushd /l4t > /dev/null
   if [[ ! -d targetfs ]]; then
     tar -I lbzip2 -xf targetfs.tbz2
+
+    unlink /l4t/targetfs/usr/bin/X11
+    unlink /l4t/targetfs/run/speech-dispatcher/.cache/speech-dispatcher
+    unlink /l4t/targetfs/run/speech-dispatcher/.speech-dispatcher
   fi
   mkdir toolchain
   tar -C toolchain -xf toolchain.tar.gz
